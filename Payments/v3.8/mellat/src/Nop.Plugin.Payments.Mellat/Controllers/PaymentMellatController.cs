f8fc5eed442333c75ea7ebdcc91e7f8c33ccb4b0
Reza Ahmadi <r.odises@gmail.com>
2016-08-28 14:08:42 +0430

    Problem with not implemented method in mellat plugin fixed. issue #10

---
+++ Payments/v3.8/mellat/src/Nop.Plugin.Payments.Mellat/Controllers/PaymentMellatController.cs
@@ -0,0 +1,230 @@
+﻿using Nop.Web.Framework.Controllers;
+using Nop.Services.Payments;
+using System.Collections.Generic;
+using Nop.Plugin.Payments.Mellat.Models;
+using Nop.Services.Orders;
+using Nop.Services.Stores;
+using Nop.Core;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using System;
+using System.Web.Mvc;
+using Nop.Core.Data;
+using Nop.Core.Domain.Orders;
+
+namespace Nop.Plugin.Payments.Mellat.Controllers
+{
+    public class PaymentMellatController : BasePaymentController
+    {
+        private readonly IStoreService _storeService;
+        private readonly IOrderService _orderService;
+        private readonly IWorkContext _workContext;
+        private readonly ISettingService _settingService;
+        private readonly ILocalizationService _localizationService;
+        private readonly ILogger _logger;
+        private readonly IOrderProcessingService _orderProcessingService;
+
+        public PaymentMellatController(
+            IStoreService storeService,
+            IWorkContext workContext,
+            ISettingService settingService,
+            ILocalizationService localizationService,
+            ILogger logger,
+            IOrderProcessingService orderProcessingService, IOrderService orderService)
+        {
+            _storeService = storeService;
+            _workContext = workContext;
+            _settingService = settingService;
+            _localizationService = localizationService;
+            _logger = logger;
+            _orderProcessingService = orderProcessingService;
+            _orderService = orderService;
+        }
+
+        [AdminAuthorize]
+        [ChildActionOnly]
+        public ActionResult Configure()
+        {
+            // load settings for a chosen store scope
+            var storeScope = GetActiveStoreScopeConfiguration(_storeService, _workContext);
+            var mellatPaymentSettings = _settingService.LoadSetting<MellatPaymentSettings>(storeScope);
+
+            var model = new ConfigurationModel();
+            model.TerminalId = mellatPaymentSettings.TerminalId;
+            model.Username = mellatPaymentSettings.Username;
+            model.Password = mellatPaymentSettings.Password;
+            
+
+            model.ActiveStoreScopeConfiguration = storeScope;
+
+            if (storeScope > 0)
+            {
+                model.TerminalId_OverrideForStore = _settingService.SettingExists(mellatPaymentSettings, x => x.TerminalId, storeScope);
+                model.Username_OverrideForStore = _settingService.SettingExists(mellatPaymentSettings, x => x.Username, storeScope);
+                model.Password_OverrideForStore = _settingService.SettingExists(mellatPaymentSettings, x => x.Password, storeScope);
+            }
+
+            return View("~/Plugins/Payments.Mellat/Views/PaymentMellat/Configure.cshtml", model);
+        }
+
+        [HttpPost]
+        [AdminAuthorize]
+        [ChildActionOnly]
+        public ActionResult Configure(ConfigurationModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Configure();
+            }
+            // load settings for a chosen store scope
+            var storeScope = GetActiveStoreScopeConfiguration(_storeService, _workContext);
+            var mellatPaymentSettings = _settingService.LoadSetting<MellatPaymentSettings>(storeScope);
+
+            //save settings
+            mellatPaymentSettings.TerminalId = model.TerminalId;
+            mellatPaymentSettings.Username = model.Username;
+            mellatPaymentSettings.Password = model.Password;
+
+
+            if (model.TerminalId_OverrideForStore || storeScope == 0)
+            {
+                _settingService.SaveSetting(mellatPaymentSettings, x => x.TerminalId, storeScope, false);
+            }
+            else if (storeScope > 0)
+            {
+                _settingService.DeleteSetting(mellatPaymentSettings, x => x.TerminalId, storeScope);
+            }
+
+            if (model.Username_OverrideForStore || storeScope == 0)
+            {
+                _settingService.SaveSetting(mellatPaymentSettings, x => x.Username, storeScope, false);
+            }
+            else if (storeScope > 0)
+            {
+                _settingService.DeleteSetting(mellatPaymentSettings, x => x.Username, storeScope);
+            }
+
+            if (model.Password_OverrideForStore || storeScope == 0)
+            {
+                _settingService.SaveSetting(mellatPaymentSettings, x => x.Password, storeScope, false);
+            }
+            else if (storeScope > 0)
+            {
+                _settingService.DeleteSetting(mellatPaymentSettings, x => x.Password, storeScope);
+            }
+
+
+            _settingService.ClearCache();
+
+            SuccessNotification(_localizationService.GetResource("Admin.Plugins.Saved"));
+
+            return Configure();
+        }
+
+        public override ProcessPaymentRequest GetPaymentInfo(FormCollection form)
+        {
+            var paymentInfo = new ProcessPaymentRequest();
+            return paymentInfo;
+        }
+
+        public override IList<string> ValidatePaymentForm(FormCollection form)
+        {
+            var warnings = new List<string>();
+            return warnings;
+        }
+
+        [ChildActionOnly]
+        public ActionResult PaymentInfo()
+        {
+            return View("~/Plugins/Payments.Mellat/Views/PaymentMellat/PaymentInfo.cshtml");
+        }
+
+        public ActionResult BeforeGateway(string refId)
+        {
+            BeforeGatewayModel model = new BeforeGatewayModel();
+            model.RefId = refId;
+            
+
+            return View("~/Plugins/Payments.Mellat/Views/PaymentMellat/BeforeGateway.cshtml", model);
+        }
+
+        [HttpPost]
+        public ActionResult MellatCallback(MellatCallbackResult data)
+        {
+            // load settings for a chosen store scope
+            var storeScope = GetActiveStoreScopeConfiguration(_storeService, _workContext);
+            var mellatPaymentSettings = _settingService.LoadSetting<MellatPaymentSettings>(storeScope);
+            var orderId = int.Parse(data.SaleOrderId.ToString());
+            var order = _orderService.GetOrderById(orderId);
+
+            if (Session["RefId"].ToString() == data.RefId)
+            {
+                if (data.ResCode == "0")
+                {
+                    var requestVerificationResult = SubmitVerifyRequest(mellatPaymentSettings.Username, 
+                        mellatPaymentSettings.Password, 
+                        long.Parse(mellatPaymentSettings.TerminalId), 
+                        orderId, 
+                        data.SaleOrderId, 
+                        data.SaleReferenceId);
+                    if (requestVerificationResult == "0")
+                    {
+                        var settlementResult = SubmitSettleRequest(long.Parse(mellatPaymentSettings.TerminalId),
+                            mellatPaymentSettings.Username, mellatPaymentSettings.Password, orderId, data.SaleOrderId,
+                            data.SaleReferenceId);
+                        if (settlementResult == "0" || settlementResult == "45")
+                        {
+                            // ok
+                            _orderProcessingService.MarkOrderAsPaid(order);
+                            return RedirectToRoute("CheckoutCompleted", new { orderId = order.Id });
+                        }
+                        else
+                        {
+                            // order cancelled
+                            _logger.Error("Settlement request failed.");
+                            return RedirectToAction("Index", "Home", new { area = "" });
+                        }
+
+                    }
+                    else
+                    {
+                        _logger.Error("Request verification failed.");
+                        return RedirectToAction("Index", "Home", new { area = "" });
+                    }
+                }
+                else if (data.ResCode == "17")
+                {
+                    _logger.Error("Request cancelled by customer.");
+                    return RedirectToAction("Index", "Home", new { area = "" });
+                }
+                else
+                {
+                    _logger.Error("ResCode != 0");
+                    return RedirectToAction("Index", "Home", new { area = "" });
+                }
+            }
+            else
+            {
+                _logger.Error("RefId mismatch.");
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+        }
+
+        protected virtual string SubmitVerifyRequest(string username, string password, long terminalId, long orderId, long saleOrderId, long saleReferenceId)
+        {
+            var bpService = new Bpm.Mellat.PaymentGatewayImplService();
+            var result = bpService.bpVerifyRequest(terminalId, username, password, orderId, saleOrderId, saleReferenceId);
+
+            return result;
+        }
+
+        protected string SubmitSettleRequest(long terminalId, string username, string password, long orderId, long saleOrderId, long saleReferenceId)
+        {
+            var bpService = new Bpm.Mellat.PaymentGatewayImplService();
+            var result = bpService.bpSettleRequest(terminalId, username, password, orderId, saleOrderId, saleReferenceId);
+
+            return result;
+        }
+    }
+}
---
+++ Payments/v3.8/mellat/src/Nop.Plugin.Payments.Mellat/MellatPaymentProcessor.cs
@@ -0,0 +1,273 @@
+﻿using Nop.Core.Plugins;
+using Nop.Services.Payments;
+using System;
+using System.Collections.Generic;
+using Nop.Core.Domain.Orders;
+using System.Web.Routing;
+using Nop.Plugin.Payments.Mellat.Controllers;
+using System.Web;
+using Nop.Core.Domain.Payments;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
+using Nop.Core;
+using System.Linq;
+using Nop.Core.Data;
+using Nop.Services.Orders;
+
+namespace Nop.Plugin.Payments.Mellat
+{
+    public class MellatPaymentProcessor : BasePlugin, IPaymentMethod
+    {
+        #region Fields
+        private readonly MellatPaymentSettings _mellatPaymentSettings;
+        private readonly IOrderService _orderService;
+        private readonly ISettingService _settingService;
+        private readonly HttpContextBase _httpContext;
+        private readonly IWebHelper _webHelper;
+        #endregion
+
+        #region Ctor
+
+        public MellatPaymentProcessor(MellatPaymentSettings mellatPaymentSettings,
+            ISettingService settingService,
+            HttpContextBase httpContext,
+            IWebHelper webHelper, IOrderService orderService)
+        {
+            this._mellatPaymentSettings = mellatPaymentSettings;
+            this._settingService = settingService;
+            this._httpContext = httpContext;
+            this._webHelper = webHelper;
+            _orderService = orderService;
+        }
+
+        #endregion
+
+        #region Methods
+        public PaymentMethodType PaymentMethodType
+        {
+            get
+            {
+                return PaymentMethodType.Redirection;
+            }
+        }
+
+        public RecurringPaymentType RecurringPaymentType
+        {
+            get
+            {
+                return RecurringPaymentType.NotSupported;
+            }
+        }
+
+        public bool SkipPaymentInfo
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool SupportCapture
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool SupportPartiallyRefund
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool SupportRefund
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool SupportVoid
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public CancelRecurringPaymentResult CancelRecurringPayment(CancelRecurringPaymentRequest cancelPaymentRequest)
+        {
+            var result = new CancelRecurringPaymentResult();
+            result.AddError("Recurring payment not supported");
+            return result;
+        }
+
+        public bool CanRePostProcessPayment(Order order)
+        {
+            if (order == null)
+                throw new ArgumentNullException("order");
+
+            //let's ensure that at least 5 seconds passed after order is placed
+            //P.S. there's no any particular reason for that. we just do it
+            if ((DateTime.UtcNow - order.CreatedOnUtc).TotalSeconds < 5)
+                return false;
+
+            return true;
+        }
+
+        public CapturePaymentResult Capture(CapturePaymentRequest capturePaymentRequest)
+        {
+            var result = new CapturePaymentResult();
+            result.AddError("Capture method not supported");
+            return result;
+        }
+
+        public decimal GetAdditionalHandlingFee(IList<ShoppingCartItem> cart)
+        {
+            return 0;
+        }
+
+        public void GetConfigurationRoute(out string actionName, out string controllerName, out System.Web.Routing.RouteValueDictionary routeValues)
+        {
+            actionName = "Configure";
+            controllerName = "PaymentMellat";
+            routeValues = new RouteValueDictionary { { "Namespaces", "Nop.Plugin.Payments.Mellat.Controllers" }, { "area", null } };
+        }
+
+        public Type GetControllerType()
+        {
+            return typeof(PaymentMellatController);
+        }
+
+        public void GetPaymentInfoRoute(out string actionName, out string controllerName, out System.Web.Routing.RouteValueDictionary routeValues)
+        {
+            actionName = "PaymentInfo";
+            controllerName = "PaymentMellat";
+            routeValues = new RouteValueDictionary { { "Namespaces", "Nop.Plugin.Payments.Mellat.Controllers" }, { "area", null } };
+        }
+
+        public bool HidePaymentMethod(IList<ShoppingCartItem> cart)
+        {
+            return false;
+        }
+
+        public void PostProcessPayment(PostProcessPaymentRequest postProcessPaymentRequest)
+        {
+            var result = SubmitPayRequest(postProcessPaymentRequest);
+
+            if (ValidatePayResponse(result))
+            {
+                var postData = PopulatePostData(result);
+
+                _httpContext.Session["RefId"] = postData.FirstOrDefault().Value;
+
+                _httpContext.Response.RedirectToRoute("MellatPaymentBeforeGateway", new { refId = postData.FirstOrDefault().Value });
+            }
+            else
+            {
+                // log
+                _httpContext.Response.RedirectToRoute("MellatPaymentFailure");
+            }
+        }
+
+        public ProcessPaymentResult ProcessPayment(ProcessPaymentRequest processPaymentRequest)
+        {
+            var result = new ProcessPaymentResult();
+            result.NewPaymentStatus = PaymentStatus.Pending;
+            return result;
+        }
+
+        public ProcessPaymentResult ProcessRecurringPayment(ProcessPaymentRequest processPaymentRequest)
+        {
+            var result = new ProcessPaymentResult();
+            result.AddError("Recurring payment not supported");
+            return result;
+        }
+
+        public RefundPaymentResult Refund(RefundPaymentRequest refundPaymentRequest)
+        {
+            var result = new RefundPaymentResult();
+            result.AddError("Refund method not supported");
+            return result;
+        }
+
+        public VoidPaymentResult Void(VoidPaymentRequest voidPaymentRequest)
+        {
+            var result = new VoidPaymentResult();
+            result.AddError("Void method not supported");
+            return result;
+        }
+
+        public override void Install()
+        {
+            var settings = new MellatPaymentSettings
+            {
+                TerminalId = "terminalId",
+                Username = "username",
+                Password = "password"
+            };
+            _settingService.SaveSetting(settings);
+
+            this.AddOrUpdatePluginLocaleResource("Plugins.Payments.Mellat.Fields.TerminalId", "Terminal Id");
+            this.AddOrUpdatePluginLocaleResource("Plugins.Payments.Mellat.Fields.Username", "Username");
+            this.AddOrUpdatePluginLocaleResource("Plugins.Payments.Mellat.Fields.Password", "Password");
+
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            _settingService.DeleteSetting<MellatPaymentSettings>();
+
+            this.DeletePluginLocaleResource("Plugins.Payments.Mellat.Fields.TerminalId");
+            this.DeletePluginLocaleResource("Plugins.Payments.Mellat.Fields.Username");
+            this.DeletePluginLocaleResource("Plugins.Payments.Mellat.Fields.Password");
+
+            base.Uninstall();
+        }
+
+        #endregion
+
+        #region Utilites
+        protected string SubmitPayRequest(PostProcessPaymentRequest postProcessPaymentRequest)
+        {
+            var order = postProcessPaymentRequest.Order;
+            var callbackUrl = string.Concat(_webHelper.GetStoreLocation(false), "payment/mellat/callback");
+
+            var payDate = DateTime.Now.Year + DateTime.Now.Month.ToString().PadLeft(2, '0') + DateTime.Now.Day.ToString().PadLeft(2, '0');
+            var payTime = DateTime.Now.Hour.ToString().PadLeft(2, '0') + DateTime.Now.Minute.ToString().PadLeft(2, '0') + DateTime.Now.Second.ToString().PadLeft(2, '0');
+
+            var bankService = new Bpm.Mellat.PaymentGatewayImplService();
+            var result = bankService.bpPayRequest(long.Parse(_mellatPaymentSettings.TerminalId), _mellatPaymentSettings.Username, _mellatPaymentSettings.Password, order.Id, long.Parse(postProcessPaymentRequest.Order.OrderTotal.ToString("#")), payDate, payTime, order.Id.ToString(), callbackUrl, 0);
+
+            return result;
+        }
+
+        protected bool ValidatePayResponse(string result)
+        {
+            var splits = result.Split(',');
+            if (splits.Length != 2 || splits[0] != "0")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        protected Dictionary<string, object> PopulatePostData(string result)
+        {
+            var parts = result.Split(',');
+            var postData = new Dictionary<string, object>
+            {
+                {"RefId", parts[1]}
+            };
+            return postData;
+        }
+        #endregion
+    }
+}
